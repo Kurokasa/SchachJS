b99bee2ea145f3866226bfff044a4634fdc34f97
Mark Czech <mark-czech@outlook.de>
2023-07-12 09:10:57 +0200

    Figuren Klassen mit Frabe und Darstellung hinzugefügt. Grundaufstellung der Felder erstellt.

--- figur.js
+++ figur.js
@@ -1,3 +1,63 @@
 class Figur{
+        constructor(){
+            let figurFarbe;
+            let darstellung;
+            const erlaubteFelder = [];
+        }
 
+}
+
+class Bauer extends Figur{
+        constructor(pos){
+            super();
+            this.darstellung = (pos.charCodeAt(1) % 2 == 0) ? "♙" : "♟︎";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 == 0) ? "weiß" : "schwarz";
+        }
+}
+
+class Springer extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = (pos.charCodeAt(1) % 2 != 0) ? "♘" : "♞";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 != 0) ? "weiß" : "schwarz";
+    }
+}
+
+class Laeufer extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = (pos.charCodeAt(1) % 2 != 0) ? "♗" : "♝";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 != 0) ? "weiß" : "schwarz";
+    }
+}
+
+class Turm extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = (pos.charCodeAt(1) % 2 != 0) ? "♖" : "♜";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 != 0) ? "weiß" : "schwarz";
+    }
+}
+
+class Dame extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = (pos.charCodeAt(1) % 2 != 0) ? "♕" : "♛";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 != 0) ? "weiß" : "schwarz";
+    }
+}
+
+class Koenig extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = (pos.charCodeAt(1) % 2 != 0) ? "♔" : "♚";
+            this.figurFarbe = (pos.charCodeAt(1) % 2 != 0) ? "weiß" : "schwarz";
+    }
+}
+
+class Leer extends Figur{
+    constructor(pos){
+        super();
+            this.darstellung = "";
+    }
 }
--- spiel.js
+++ spiel.js
@@ -1,8 +1,12 @@
 class spiel{
     constructor(){
         this.zugNr = 0;
+        this.brett;
         this.p1 = new spieler("Weiß", true)
         this.p2 = new spieler("Schwarz", false)
+    }
+    neuesSpiel(){
+        this.brett = new brett()
     }
 }
 
@@ -16,10 +20,40 @@ class spieler{
 class brett{
     spielfeld = new Map();
     constructor(){
-        for (let z = 1; z <= 8; z++){
+        for (let z = 8; z >= 1; z--){
+            if (z == 8){
+                for (let i = 0; i < 10; i++){
+                    let randFeld = document.createElement("button");
+                    randFeld.className = "feld rand";
+                    if (!(i == 0 || i == 9))
+                        randFeld.innerHTML = String.fromCharCode(64 + i);
+                    document.getElementById("spielfeld").appendChild(randFeld);
+                }
+            }
             for (let s = "A"; s <= "H"; s = String.fromCharCode(s.charCodeAt(0) + 1)){
+                if (s == "A"){
+                    let randFeld = document.createElement("button");
+                    randFeld.className = "feld rand";
+                    randFeld.innerHTML = z;
+                    document.getElementById("spielfeld").appendChild(randFeld);
+                }
                 let key = s + z;
                 this.spielfeld[key] = new feld(key)
+                if (s == "H"){
+                    let randFeld = document.createElement("button");
+                    randFeld.className = "feld rand";
+                    randFeld.innerHTML = z;
+                    document.getElementById("spielfeld").appendChild(randFeld);
+                }
+            }
+            if (z == 1){
+                for (let i = 0; i < 10; i++){
+                    let randFeld = document.createElement("button");
+                    randFeld.className = "feld rand";
+                    if (!(i == 0 || i == 9))
+                        randFeld.innerHTML = String.fromCharCode(64 + i);
+                    document.getElementById("spielfeld").appendChild(randFeld);
+                }
             }
         }
     }
@@ -27,10 +61,43 @@ class brett{
 
 class feld{
     constructor(pos){
+        this.pos = pos
         this.feldFarbe = ((pos.charCodeAt(0) + pos.charCodeAt(1)) % 2 == 0) ? "black" : "white";
-        const htmlFeld = document.createElement("button");
-        htmlFeld.innerHTML = (this.feldFarbe == "black") ? "♖" : "♜";
-        htmlFeld.className = "feld " + this.feldFarbe;
-        document.getElementById("spielfeld").appendChild(htmlFeld)
+        this.feldButton = document.createElement("button");
+        this.feldButton.className = "feld " + this.feldFarbe;
+
+        switch(pos.charCodeAt(1)){
+            case 55: // -> 7
+            case 50: // -> 2
+                this.figur = new Bauer(pos);
+                break;
+            case 56: // -> 8
+            case 49: // -> 1
+                switch(pos.charCodeAt(0)){
+                    case 65: // -> A
+                    case 72: // -> H
+                        this.figur = new Turm(pos);
+                        break;
+                    case 66: // -> B
+                    case 71: // -> G
+                        this.figur = new Springer(pos);
+                        break;
+                    case 67: // -> C
+                    case 70: // -> F
+                        this.figur = new Laeufer(pos);
+                        break;
+                    case 68: // -> D
+                        this.figur = new Koenig(pos);
+                        break;
+                    case 69: // -> E
+                        this.figur = new Dame(pos);
+                }
+                break;
+            default:
+                this.figur = new Leer();
+        }
+        
+        this.feldButton.innerHTML = this.figur.darstellung;
+        document.getElementById("spielfeld").appendChild(this.feldButton);
     }
 }
